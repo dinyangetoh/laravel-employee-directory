c16a2e1cd8ecc1e4119a1aa9d3d73aa678f59130
David Inyang-Etoh <dinyangetoh@gmail.com>
2017-08-20 09:00:02 +0100

    Enhanced company view page for Department and employee lists

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -57,6 +57,7 @@
 
     </div>
 
+   
 @endsection
 
 @section('sidebar')
--- resources/views/companies/view.blade.php
+++ resources/views/companies/view.blade.php
@@ -14,4 +14,37 @@
 
     </div>
 
+    <!-- List Departments -->
+     <div class='panel panel-default'>
+        <div class='panel-heading'>
+            <h4>List Employees
+                <div class="pull-right">
+                    <button class='btn btn-primary'>Add Employee</button>
+                </div>
+            </h4>
+        </div>
+
+        <div class="panel-body">
+            <p>No records found</p>
+        </div>
+    </div>
+
+
+@endsection
+  
+@section('sidebar')
+      <!-- List Departments -->
+     <div class='panel panel-default'>
+        <div class='panel-heading'>
+            <h4>Departments
+                <div class="pull-right">
+                    <button class='btn btn-primary'>Add</button>
+                </div>
+            </h4>
+        </div>
+
+        <div class="panel-body">
+            <p>No records found</p>
+        </div>
+    </div>
 @endsection
